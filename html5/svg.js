7f25617f72de3add776ed460fda1281de81e1573
GabrielClaudiu <gacl20@yahoo.com>
2018-12-03 12:27:09 +0200

    Update svg.js

--- html5/svg.js
+++ html5/svg.js
@@ -1,17 +1,17 @@
 document.getElementById("id_bussiness_version").innerHTML = "Business version: 2018.11.5.3";
 
 window.addEventListener("deviceorientation", on_device_orientation);
-window.addEventListener("devicemotion", on_device_motion);
+//window.addEventListener("devicemotion", on_device_motion);
 
 //--------------------------------------------------
 function on_device_orientation(e)
 {
-	var circle = document.getElementById("id_circle");
 	var svg = document.getElementById("id_svg");
+	var circle = document.getElementById("id_circle");
 	
 	var R = 20;
 	
-	circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R));
-	circle.setAttribute("cy", svg.height / 2 + e.beta / 90 * (svg.height / 2 - R));
+	circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R) + "px");
+	circle.setAttribute("cy", svg.height / 2 + e.beta / 90 * (svg.height / 2 - R) + "px");
 }
 //--------------------------------------------------
