d26863202a97b5f3b75daac469111a8dc20804d6
GabrielClaudiu <gacl20@yahoo.com>
2018-12-03 12:40:30 +0200

    Update svg.js

--- html5/svg.js
+++ html5/svg.js
@@ -11,7 +11,9 @@ function on_device_orientation(e)
 	
 	var R = 20;
 	
-	circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R) + "px");
-	circle.setAttribute("cy", svg.height / 2 + e.beta / 90 * (svg.height / 2 - R) + "px");
+	var svg_width = svg.getAttention("width");
+	var svg_height = svg.getAttention("height");
+	circle.setAttribute("cx", svg_width / 2 + e.gamma / 90 * (svg_width / 2 - R) + "px");
+	circle.setAttribute("cy", svg_height / 2 + e.beta / 90 * (svg_height / 2 - R) + "px");
 }
 //--------------------------------------------------
