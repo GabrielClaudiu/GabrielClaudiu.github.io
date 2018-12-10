ff503d15a43e22a8dab6015faf60c7ad5e42c80d
GabrielClaudiu <gacl20@yahoo.com>
2018-12-10 11:43:13 +0200

    up

--- html5/synth.js
+++ html5/synth.js
@@ -1,8 +1,10 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.10.0";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.10.1";
 document.getElementById("id_get_voices").addEventListener("click", get_voices);
+document.getElementById("id_start").addEventListener("click", speak);
 
 var synth = window.speechSynthesis;
 
+//-------------------------------------------
 function get_voices()
 {
 	var voices = synth.getVoices();
@@ -10,3 +12,13 @@ function get_voices()
 	for (var i = 0; i < voices.lenght; i++)
 		voices_list.innerHTML += voices[i].lang + " " + voices[i].name + "<br>";
 }
+//-------------------------------------------
+function speak()
+{
+	var utterance = new SpeechSynthesisUtterance();
+	utterance.lang = "en-US";
+	uttrance.text = document.getElementById("id_text").value;
+	
+	synth.speak(utterance);
+}
+//-------------------------------------------
